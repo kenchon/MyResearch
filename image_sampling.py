b5010d17aad8ceb11cef13f5a1fb4ac6eddaa15a
kenmikanmi <ken-h5526@live.jp>
2018-02-04 21:58:53 +0900

    Add files via upload
    Added train code 'train_gpu.py' for GPU computation and 'train_exception.py' includes modification in order to deal with error caused by illegal format image.

--- image_sampling.py
+++ image_sampling.py
@@ -1,4 +1,56 @@
 from torch import Tensor
+from torchvision import models, transforms
+import torch
+import csv
+from PIL import Image
 
-def triplet_sampling(batch_size = 1):
-    return img_ref, img_pos, img_neg
+f_photos = open("photos.txt","r")
+photos = f_photos.readlines()
+
+
+def img2tensor(pixel_img):
+    normalize = transforms.Normalize(
+        mean=[0.5657177752729754, 0.5381838567195789, 0.4972228365504561],
+        std=[0.29023818639817184, 0.2874722565279285, 0.2933830104791508]
+    )
+    preprocess = transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    return preprocess(pixel_img)
+
+
+def triplet_sampling(batch_size, csv_index):
+    f_sim = open("triplet.csv","r")
+    reader_csv = csv.reader(f_sim)
+
+    reader = []
+    count = 0
+    for i in reader_csv:
+        if count >= csv_index and count < csv_index + batch_size:
+            reader.append(i)
+        count += 1
+    f_sim.close()
+
+    ref_tensor = pos_tensor = neg_tensor = torch.Tensor(batch_size,3,384,256)
+
+    sim_pos = []
+    sim_neg = []
+
+    count = 0
+    for row in reader:
+        ref_tensor[count] = img2tensor(Image.open(photos[int(row[0])].strip()))
+        pos_tensor[count] = img2tensor(Image.open(photos[int(row[1])].strip()))
+        neg_tensor[count] = img2tensor(Image.open(photos[int(row[2])].strip()))
+        sim_pos.append(float(row[3]))
+        sim_neg.append(float(row[4]))
+        count += 1
+        if count == batch_size:
+            break
+
+    img = [ref_tensor,pos_tensor,neg_tensor]
+    sim = [sim_pos,sim_neg]
+    #print("csv_index:\t"+str(csv_index))
+
+    return img, sim, csv_index+batch_size
---
+++ train_gpu.py
@@ -0,0 +1,62 @@
+import load_model
+import image_sampling
+import torch
+from torch import exp
+from torch.autograd import Variable
+from torch import autograd
+from image_sampling import triplet_sampling
+
+def triplet_loss(feat, sim):
+
+    dist_pos = torch.norm(feat[0] - feat[1], 2)
+    dist_neg = torch.norm(feat[0] - feat[2], 2)
+
+    reg_dist_pos = exp(dist_pos)/(exp(dist_pos) + exp(dist_neg))
+    reg_dist_neg = exp(dist_neg)/(exp(dist_pos) + exp(dist_neg))
+
+    loss = ((sim[0] - reg_dist_pos)**2 + (sim[1] - reg_dist_neg)**2)/2
+
+    return loss
+
+
+if __name__ == "__main__":
+    csv_index = 0
+
+    model = load_model.model
+    model.cuda()
+
+    learning_rate = 1e-2
+    epoch = 1000
+    optimizer = torch.optim.Adadelta(model.parameters(),lr=learning_rate,  weight_decay=0)
+    # model.train(False)    # use network as feature extractor
+    batch_size = 4
+
+    for t in range(epoch):
+        try:
+            csv_index = 0
+
+            loss = 0
+            row = 0
+            # img, sim are list where i th column holds i th triplet
+            img, sim, csv_index = triplet_sampling(batch_size, csv_index)
+            #print("train.py csv_index:"+str(csv_index))
+
+            #print("length of img\t"+str(len(img)))
+            #print("length of sim\t"+str(len(sim)))
+
+            for i in range(batch_size):
+                feat = []
+                for j in range(3):
+                    feat.append(model.forward(Variable(img[j][i].unsqueeze_(0).cuda())))
+                loss += triplet_loss(feat, (sim[0][i],sim[1][i]))
+
+
+            loss /= batch_size
+            print(t, loss.data[0])
+
+            optimizer.zero_grad()
+
+            loss.backward()
+            optimizer.step()
+        except:
+            csv_index += 0
